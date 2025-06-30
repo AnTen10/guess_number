99491ae600bd59e9e8c1da8ed8c5ff1e79ebb3ce
Анастасия Тен <riciticiten@yandex.ru>
2025-06-30 19:40:19 +0300

    optimize key logic

--- guess_number.py
+++ guess_number.py
@@ -1,4 +1,3 @@
-from asyncore import write
 from random import randint
 number = randint(1, 100)
 print('Угадай число от 1 до 100')
@@ -7,8 +6,8 @@ while True:
     guess = int(input('Введите число: '))
     if guess < number:
         print('Ваше число меньше того, что загадано.')
-    if guess > number:
+    elif guess > number:
         print('Ваше число больше того, что загадано.')
-    if guess == number:
+    elif guess == number:
         break
 print('Отличная интуиция! Вы угадали число :)')
